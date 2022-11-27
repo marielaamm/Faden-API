ff999f2404236a5c789f1db01d263a54b170d3f2
carolfp1974 <carolflorespe@gmail.com>
2022-11-26 19:54:28 -0600

    Paciente controller

---
+++ Faden-Api/Class/cat/Cls_Paciente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Faden_Api.Class.cat
+{
+    public class Cls_Paciente
+    {
+        public int IdPaciente = 0;
+        public string NoExpediente = string.Empty;
+        public DateTime FechaIngreso = DateTime.Now;
+        public string PNombre = string.Empty;
+        public string SNombre = string.Empty;
+        public string PApellido = string.Empty;
+        public string SApellido = string.Empty;
+        public string Sexo = string.Empty;
+        public int IdDepto = 0;
+        public int IdCiudad = 0;
+        public DateTime FechaNacim = DateTime.Now;
+        public string Ocupacion = string.Empty;
+        public string Identificacion = string.Empty;
+        public int IdEscolaridad = 0;
+        public string ECivil = string.Empty;
+        public string Direccion = string.Empty;
+        public string Telefono = string.Empty;
+        public string Celular = string.Empty;
+        public string Correo = string.Empty;
+        public string Religion = string.Empty;
+        public string Convive = string.Empty;
+        public string Visita = string.Empty;
+        public string RefVisita = string.Empty;
+        public string Referencia = string.Empty;
+        public string Trabaja = string.Empty;
+        public string RefTrabajo = string.Empty;
+        public string UltimoTrabajo = string.Empty;
+        public string RefUltTrabajo = string.Empty;
+        public string Jubilado = string.Empty;
+        public string Pensionado = string.Empty;
+        public string Estado = string.Empty;
+
+    }
+}
