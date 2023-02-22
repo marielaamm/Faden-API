dfb14cfb0fbb66bcebced9b9c3ab682a09c5754b
carolfp1974 <carolflorespe@gmail.com>
2023-02-21 21:19:12 -0600

    clase sindromepredominante clase conseso

---
+++ Faden-Api/Class/cat/Cls_Consenso.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Faden_Api.Class.cat
+{
+    public class Cls_Consenso
+    {
+        public string RdDetCognitivo = string.Empty;
+        public string RdSospechaDiag = string.Empty;
+        public string RefNormal = string.Empty;
+        public string RefLeve = string.Empty;
+        public string RefMayor = string.Empty;
+        public string Depresion = string.Empty;
+        public string RefDepresion = string.Empty;
+        public string TrastornoBip = string.Empty;
+        public string RefTrasBip = string.Empty;
+        public string Esquizo = string.Empty;
+        public string RefEsquizo = string.Empty;
+        public string OtroDiag = string.Empty;
+        public string RefOtroDiag = string.Empty;
+        public string RefProbable = string.Empty;
+        public string RefConfirmado = string.Empty;
+        public string TrataFarma = string.Empty;
+        public string TrataNoFarma = string.Empty;
+        public string Recomendaciones = string.Empty;
+        public string Examenes = string.Empty;
+        public int IdPaciente = 0;
+        public Cls_SindromePredominante[] SindromePredominante = null;
+
+
+
+    }
+}
---
+++ Faden-Api/Class/cat/Cls_SindromePredominante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Faden_Api.Class.cat
+{
+    public class Cls_SindromePredominante
+    {
+        public int IdSindrome = 0;
+        public string TipoSindrome = string.Empty;
+        public int IdPaciente = 0;
+    }
+}
