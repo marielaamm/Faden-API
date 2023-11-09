c888eeed4e7ca7d4b373f8b1808a9757fa1a2cc9
JairMartinez86 <jmartinezg86@gmail.com>
2023-11-09 08:44:13 -0600

    Reporte

---
+++ Faden-Api/Controllers/EXP/ImprimirController.cs
@@ -0,0 +1,47 @@
+﻿using Faden_Api.Class;
+using Faden_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace Faden_Api.Controllers.EXP
+{
+    public class ImprimirController : ApiController
+    {
+        [Route("api/Reporte/Imprimir")]
+        [HttpGet]
+        public string Imprimir(string op)
+        {
+            return _Imprimir(op);
+        }
+
+        private string _Imprimir(string op)
+        {
+            string json = string.Empty;
+
+
+
+            try
+            {
+                using (FADENEntities _conexion = new FADENEntities())
+                {
+                    
+                   
+                }
+            }
+
+            catch (Exception ex)
+            {
+                json = Cls_Mensaje.Tojson(null, 0, "1", ex.Message, 1);
+            }
+
+            return json;
+        }
+
+    }
+}
